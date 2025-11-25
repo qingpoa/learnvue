0b3d368282b52a987bd8141d17cb2661bc05d2d6
qingpoa <senqibusenqi@163.com>
2025-11-25 17:48:48 +0800

    生命周期

--- learnvue/src/App.vue
+++ learnvue/src/App.vue
@@ -1,23 +1,14 @@
 <template>
   <div class = "content">
-    <Person :list="list"/>
+    <Person v-if = "isshow"/>
   </div>
 </template>
 
 <script lang="ts" setup name="App">
-  import { reactive } from 'vue';
-import Person from './components/person.vue'
-import type { Persons } from './types';
-  let list = reactive<Persons>([{
-    name:"张三",
-    age:18,
-    id:1
-  },
-  {
-    name:"李四",
-    age:18,
-    id:2
-  }])
+  import { reactive,ref } from 'vue';
+  import Person from './components/person.vue'
+  import type { Persons } from './types';
+  let isshow = ref(true)
 </script>
 
 <style scoped>
--- learnvue/src/components/person.vue
+++ learnvue/src/components/person.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="Person">
-    <ul>
-      <li v-for="item in a.list" :key = "item.id">姓名：{{ item.name }}-年龄：{{ item.age }}</li>
-    </ul>
+    <h2>{{sum}}</h2>
+    <button @click="addsum">点击sum加1</button>
   </div>
 </template>
 
 
 <script setup lang = 'ts' name="Person">
-  import { ref , computed, watch, reactive, watchEffect} from 'vue'
-  import { type person, type Persons }  from "../types"
-  let a = defineProps<{list?:Persons}>()
+  import {ref, onBeforeMount, onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted} from 'vue'
+  let sum = ref(0)
+  function addsum()
+  {
+    sum.value += 1
+  } 
+  console.log("创建")
+  onBeforeMount(() => {
+    console.log("挂载前")
+  })
+  onMounted(()=>{
+    console.log("挂载后")
+  })
+  onBeforeUpdate(()=>{
+    console.log("更新前")
+  })
+  onUpdated(()=>{
+    console.log("更新完毕")
+  })
+  onBeforeUnmount(()=>{
+    console.log("卸载前")
+  })
+  onUnmounted(()=>{
+    console.log("卸载后")
+  })
 </script>
 
 
