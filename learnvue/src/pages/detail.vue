97b20ed685562363a8663319a7c91c687010c1d8
qingpoa <senqibusenqi@163.com>
2026-01-29 01:54:59 +0800

    query参数

--- learnvue/src/pages/detail.vue
+++ learnvue/src/pages/detail.vue
@@ -1,12 +1,15 @@
 <template>
   <ul class="news-list">
-    <li>编号：xxx</li>
-    <li>标题：xxx</li>
-    <li>内容：xxx</li>
+    <li>编号:{{ route.query.id }}</li>
+    <li>标题:{{ route.query.title }}</li>
+    <li>内容:{{ route.query.content }}</li>
   </ul>
 </template>
 
 <script setup lang="ts" name="About">
+  import { useRoute } from 'vue-router';
+  let route = useRoute()
+
 
 </script>
 
