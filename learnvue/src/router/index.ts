2684db4fa3c9088f39334f59ff1c82b121821746
qingpoa <senqibusenqi@163.com>
2026-02-01 19:21:35 +0800

    重定向

--- learnvue/src/router/index.ts
+++ learnvue/src/router/index.ts
@@ -38,6 +38,10 @@ const router = createRouter(
             {
                 path:"/about",
                 component:about
+            },
+            {
+                path:"/",
+                redirect:"/home"
             }
         ]
     }
